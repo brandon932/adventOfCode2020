5af95302a1801543f8f282bcc065bdfd6962e9d6
brandon932 <bthornton_932@yahoo.com>
2020-12-12 12:44:31 -0700

    updated function to take perams for part two. Working but produces the wrong result

--- 3/tobogganTrajectory.py
+++ 3/tobogganTrajectory.py
@@ -7,7 +7,7 @@ def getData():
             dataRow = list(row[0])
             data.append(dataRow)
         return data
-def findTrees():
+def findTrees(x,y):
     lineCount = 0
     length = 31
     treeCount = 0
@@ -16,11 +16,23 @@ def findTrees():
     for row in data:
         lineCount += 1
         if row[yCount] == "#":
-            treeCount += 1
-        yCount += 3
+            #something wrong here I think. value too high
+            if lineCount % x == 0:
+                treeCount += 1
+        if lineCount % x == 0:
+            yCount += y
         if yCount >= 30:
             yCount -= length
             #print "resetline :" + str(count) + " y :" + str(yCount) + " tree Count :"+ str(treeCount)
     print treeCount
     return treeCount
-findTrees()
+
+def calculate_paths():
+    paths = [[1,1],[1,3],[1,5],[1,7],[2,1]]
+    pathProduct = 1
+    for path in paths:
+        pathProduct *= findTrees(path[0],path[1])
+    print pathProduct
+    return pathProduct
+
+calculate_paths()
