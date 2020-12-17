ba5d35d844f9a903408dcbe541b7b9fb6812d4f4
brandon932 <bthornton_932@yahoo.com>
2020-12-17 16:49:26 -0700

    Finally validated passports correctly

--- 4/passportProcessing.py
+++ 4/passportProcessing.py
@@ -1,4 +1,5 @@
 import csv
+import re
     #byr (Birth Year)
     #iyr (Issue Year)
     #eyr (Expiration Year)
@@ -21,28 +22,44 @@ def getPassportData():
                 passport[key] = value
             passportList.append(passport)
         #print passportList
-        return passportList
-def validatePassport(passport):
-    valid = 1
-    #hgt (Height)
-    #hcl (Hair Color)
-    #ecl (Eye Color)
-    #pid (Passport ID)
     byr = int(passport['byr'])
     if not(byr >= 1920 and byr <= 2002):
         valid = 0
+
     iyr = int(passport['iyr'])
     if not(iyr >= 2010 and iyr <= 2020):
         valid = 0
+
     eyr = int(passport['eyr'])
     if not(eyr >= 2020 and eyr <= 2030):
         valid = 0
+
     hgt = passport['hgt']
     if hgt.endswith('in'):
-        valid = 1
+        hgt = int(hgt[0:-2])
+        if not(hgt>= 59 and hgt <= 76):
+            valid = 0
+
     elif hgt.endswith('cm'):
-        valid = 1
+        hgt = int(hgt[0:-2])
+        if not(hgt >= 150 and hgt <= 193):
+            valid = 0
     else:
+        valid = 0
+
+    hcl = passport['hcl']
+    match = re.findall("(#[A-Fa-f0-9]{6})", hcl)
+    if not(len(match)) > 0 :
+        valid = 0
+
+    eyeColor = {'amb','blu','brn','gry','grn','hzl','oth'}
+    ecl = {passport['ecl']}
+    if not(ecl.issubset(eyeColor)):
+       valid = 0
+
+    pid = passport['pid']
+    validId = len(pid) == 9
+    if not(validId):
         valid = 0
     return valid
 
@@ -52,7 +69,7 @@ def processPassports():
     goodPassports = 0
     for passport in passportData:
         if keys.issubset(passport.keys()):
-            goodPassports += 1# validatePassport(passport)
+            goodPassports += validatePassport(passport)
     print goodPassports
     return goodPassports
 
