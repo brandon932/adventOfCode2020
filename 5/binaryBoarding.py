74d5e3c8c6afadb160767a92c927cc528ae8b25d
brandon932 <bthornton_932@yahoo.com>
2020-12-19 12:43:37 -0700

    solved part one day 5

--- 5/binaryBoarding.py
+++ 5/binaryBoarding.py
@@ -1,1 +1,28 @@
-print "hello world" 
+def scanData():
+    with open("data.csv") as input_file:
+        data_str = input_file.read()
+        bordingPassArray = data_str.split('\n')
+        return bordingPassArray
+
+def convertToInt(binaryString):
+    string = binaryString.replace("F","0")
+    string = string.replace("B","1")
+    string = string.replace("L","0")
+    string = string.replace("R","1")
+    return int(string, 2)
+
+def getBordingPassId():
+    higestPassId = 0
+    passList = scanData()
+    for data in passList:
+        row = convertToInt(data[:7])
+        seat = convertToInt(data[-3:])
+        passId = (row * 8) + seat
+        if passId> higestPassId:
+            higestPassId = passId
+        print higestPassId
+    return higestPassId
+
+getBordingPassId()
+
+
