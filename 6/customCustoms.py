7a223b9e28e3425efb3fe5d510e980b83bbb5f73
brandon932 <bthornton_932@yahoo.com>
2020-12-22 14:56:47 -0700

    completed day 6 part 1

---
+++ 6/customCustoms.py
@@ -0,0 +1,21 @@
+def getData():
+    with open("data.csv") as data_file:
+        data_str = data_file.read();
+        dataArray = data_str.split('\n\n')
+        customsArray = [filter(None,data.split('\n')) for data in dataArray]
+        return customsArray
+def getCustomsLength(customsSheet):
+    customSet = set()
+    for sheet in customsSheet:
+        customSet.update(set(sheet))
+    return len(customSet)
+
+
+def getTotalCustoms():
+    count = 0
+    customsArray = getData()
+    for customsSheet in customsArray:
+        count += getCustomsLength(customsSheet)
+    print count
+    return count
+getTotalCustoms()
